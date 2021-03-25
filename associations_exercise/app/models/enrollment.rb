cb424b285f4a1a9f87d837074351a98ad5684ff1
Syldys <syldysnya@gmail.com>
2021-03-25 14:50:52 -0400

    Add changes to model user.rb

--- associations_exercise/app/models/enrollment.rb
+++ associations_exercise/app/models/enrollment.rb
@@ -1,2 +1,29 @@
 class Enrollment < ApplicationRecord
+
 end
+
+
+# class User < ApplicationRecord
+#   has_many :likes,
+#   	primary_key: :id,
+#   	foreign_key: :liker_id,
+#   	class_name: :Like
+# end
+
+# class User < ApplicationRecord
+#   has_many :liked_songs,
+#       through: :likes,
+#       source: :song
+# end
+
+# class Song < ApplicationRecord
+#   belongs_to :author,
+#   	primary_key: :id,
+#   	foreign_key: :author_id,
+#   	class_name: :User
+# end
+
+# validates :some_column, presence: true
+#   similar to null: false
+# validates :other_column, uniqueness: true
+#   similar to unique: true
--- associations_exercise/app/models/user.rb
+++ associations_exercise/app/models/user.rb
@@ -1,2 +1,37 @@
 class User < ApplicationRecord
+    validates :name, presence: true
+
+    has_many :attending_courses,
+        through: :enrollments,
+        source: :course
+    
+    has_many :teaching_courses,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Course
 end
+
+# class User < ApplicationRecord
+#   has_many :likes,
+#   	primary_key: :id,
+#   	foreign_key: :liker_id,
+#   	class_name: :Like
+# end
+
+# class User < ApplicationRecord
+#   has_many :liked_songs,
+#       through: :likes,
+#       source: :song
+# end
+
+# class Song < ApplicationRecord
+#   belongs_to :author,
+#   	primary_key: :id,
+#   	foreign_key: :author_id,
+#   	class_name: :User
+# end
+
+# validates :some_column, presence: true
+#   similar to null: false
+# validates :other_column, uniqueness: true
+#   similar to unique: true
